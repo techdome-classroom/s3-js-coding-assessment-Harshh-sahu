ef8654ddeb3e72e6ba81c4d5e2a6c07863c14347
Harsh sahu <116263893+Harshh-sahu@users.noreply.github.com>
2024-10-07 15:23:15 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,9 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
+// program1.js
 var isValid = function(s) {
-    
+    const stack = [];
+    const map = { ')': '(', '}': '{', ']': '[' };
+
+    for (let char of s) {
+        if (map[char]) {
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
 };
+
+module.exports = { isValid };
+
 
 module.exports = { isValid };
 
