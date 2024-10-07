3d280571d857d15b55fc22cf4bdac83ba8663f35
Harsh sahu <116263893+Harshh-sahu@users.noreply.github.com>
2024-10-07 15:18:13 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,35 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    // Stack to keep track of opening brackets
+    const stack = [];
     
+    // Mapping of closing brackets to their corresponding opening brackets
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    // Loop through each character in the string
+    for (let char of s) {
+        // If it's a closing bracket
+        if (map[char]) {
+            // Pop the top element from the stack, or use a dummy value if stack is empty
+            const topElement = stack.length > 0 ? stack.pop() : '#';
+            
+            // If the popped element doesn't match the corresponding opening bracket
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+            // If it's an opening bracket, push it onto the stack
+            stack.push(char);
+        }
+    }
+
+    // If the stack is empty, all brackets were properly matched
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
